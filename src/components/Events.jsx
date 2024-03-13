bd1d82467f8c23129761eeb0ecba01a400649bce
aryaa9 <aryaman.singh214@gmail.com>
2024-03-13 00:06:47 -0400

    event changes

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import EventImage from '../assets/Events.jpg';
+import EventImage from '../assets/noevent.jpeg';
 
 const Events = () => {
   return (
@@ -22,29 +22,7 @@ const Events = () => {
           <p className='text-[#00df9a] font-bold'>UPCOMING EVENT</p>
           <h2 className='text-xl font-bold py-2'>Event Name</h2>
           <p className='text-gray-700'>
-            Description of the event goes here.
-          </p>
-          <button className='bg-[#00df9a] text-black w-full rounded-md font-medium my-6 py-3'>Explore Event</button>
-        </div>
-
-        {/* Second Event Card */}
-        <div className='event-card bg-gray-100 rounded-lg p-6'>
-          <img className='w-full mb-4' src={EventImage} alt='Event Photo' />
-          <p className='text-[#00df9a] font-bold'>UPCOMING EVENT</p>
-          <h2 className='text-xl font-bold py-2'>Event Name</h2>
-          <p className='text-gray-700'>
-            Description of the event goes here.
-          </p>
-          <button className='bg-[#00df9a] text-black w-full rounded-md font-medium my-6 py-3'>Explore Event</button>
-        </div>
-
-        {/* Third Event Card */}
-        <div className='event-card bg-gray-100 rounded-lg p-6'>
-          <img className='w-full mb-4' src={EventImage} alt='Event Photo' />
-          <p className='text-[#00df9a] font-bold'>UPCOMING EVENT</p>
-          <h2 className='text-xl font-bold py-2'>Event Name</h2>
-          <p className='text-gray-700'>
-            Description of the event goes here.
+            No scheduled Events for now, Come back later 👨‍💻👨‍💻
           </p>
           <button className='bg-[#00df9a] text-black w-full rounded-md font-medium my-6 py-3'>Explore Event</button>
         </div>
